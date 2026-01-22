8312464284a801ad1e25d8abeeaa862ff6d27aa8
keqian <765197310@qq.com>
2026-01-22 14:28:15 +0800

    feat: css->less

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/router.config';
-import './styles.css';
+import './styles.less';
 import './global.less';
 
 createRoot(document.getElementById('root')).render(<RouterProvider router={router} />);
